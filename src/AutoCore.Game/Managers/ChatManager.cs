5a6014390593b7a6e12c8e78bb5dc2183012cdc6
Blumster <sarkany0@hotmail.com>
2023-04-29 23:54:38 +0200

    Create ChatManager skeleton

---
+++ src/AutoCore.Game/Managers/ChatManager.cs
@@ -0,0 +1,21 @@
+﻿namespace AutoCore.Game.Managers;
+
+using AutoCore.Game.Packets.Global;
+using AutoCore.Game.Packets.Sector;
+using AutoCore.Game.TNL;
+using AutoCore.Utils.Memory;
+
+public class ChatManager : Singleton<ChatManager>
+{
+    public void HandleChat(TNLConnection connection, BinaryReader reader)
+    {
+        // TODO: load packet then send it back to the sender
+        // TODO: later: handle chat commands and send the chat packet to the proper recipient(s)
+    }
+
+    public void HandleBroadcast(TNLConnection connection, BinaryReader reader)
+    {
+        // TODO: load packet then send it back to the sender
+        // TODO: later: handle chat commands and send the chat packet to the proper recipient(s)
+    }
+}
