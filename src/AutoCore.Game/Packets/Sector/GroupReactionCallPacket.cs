459ceb1e1c60c757a61f63b309c5070ba3f3f0a8
Blumster <sarkany0@hotmail.com>
2023-05-27 02:23:54 +0200

    Implement LogicStateChange and GroupReactionCall packets

---
+++ src/AutoCore.Game/Packets/Sector/GroupReactionCallPacket.cs
@@ -0,0 +1,58 @@
+﻿using TNL.Utils;
+
+namespace AutoCore.Game.Packets.Sector;
+
+using AutoCore.Game.Constants;
+using AutoCore.Game.Extensions;
+using AutoCore.Game.Structures;
+
+public class GroupReactionCallPacket : BasePacket
+{
+    public override GameOpcode Opcode => GameOpcode.GroupReactionCall;
+
+    private List<LogicStateChangePacket> Packets { get; } = new();
+
+    public bool AddPacket(LogicStateChangePacket packet)
+    {
+        if (Packets.Count == 255)
+            return false;
+
+        Packets.Add(packet);
+        return true;
+    }
+
+    // NOTE: this is not the real layout of the packing, but it is manually packed!
+    public override void Read(BinaryReader reader)
+    {
+        throw new NotSupportedException();
+    }
+
+    // NOTE: this is not the real layout of the packing, but it is manually packed!
+    public override void Write(BinaryWriter writer)
+    {
+        var stream = new BitStream();
+        stream.WriteInt((uint)(Packets.Count & 0xFF), 8);
+
+        foreach (var packet in Packets)
+        {
+            stream.WriteInt((uint)packet.Type, 8);
+
+            if (packet.Type == LogicStateChangeType.Variable)
+            {
+                stream.WriteInt((uint)(packet.VariableId & 0xFFFF), 16);
+                stream.Write(packet.Value);
+            }
+            else if (packet.Type == LogicStateChangeType.Reaction)
+            {
+                stream.WriteInt((uint)packet.ReactionCoid, 19);
+                stream.Write(packet.Activator.Coid);
+                stream.WriteFlag(packet.Activator.Global);
+                stream.WriteFlag(packet.SingleClientOnly);
+            }
+            else
+                throw new InvalidDataException($"Unknown LogicStateChangeType {packet.Type}!");
+        }
+
+        writer.Write(stream.GetBuffer(), 0, (int)stream.GetBytePosition());
+    }
+}
---
+++ src/AutoCore.Game/Packets/Sector/LogicStateChangePacket.cs
@@ -0,0 +1,69 @@
+﻿namespace AutoCore.Game.Packets.Sector;
+
+using AutoCore.Game.Constants;
+using AutoCore.Game.Extensions;
+using AutoCore.Game.Structures;
+
+public enum LogicStateChangeType
+{
+    Reaction = 0,
+    Variable = 1
+}
+
+public class LogicStateChangePacket : BasePacket
+{
+    public override GameOpcode Opcode => GameOpcode.LogicStateChange;
+
+    public LogicStateChangeType Type { get; set; }
+
+    public int VariableId { get; set; }
+    public float Value { get; set; }
+
+    public long ReactionCoid { get; set; }
+    public TFID Activator { get; set; }
+    public bool SingleClientOnly { get; set; }
+
+    public LogicStateChangePacket(int variableId, float value)
+    {
+        Type = LogicStateChangeType.Variable;
+        VariableId = variableId;
+        Value = value;
+    }
+
+    public LogicStateChangePacket(long reactionCoid, TFID activator, bool singleClientOnly)
+    {
+        ReactionCoid = reactionCoid;
+        Activator = activator;
+        SingleClientOnly = singleClientOnly;
+    }
+
+    public override void Read(BinaryReader reader)
+    {
+        throw new NotSupportedException();
+    }
+
+    public override void Write(BinaryWriter writer)
+    {
+        writer.Write((byte)Type);
+
+        writer.BaseStream.Position += 3;
+
+        if (Type == LogicStateChangeType.Variable)
+        {
+            writer.Write(VariableId);
+            writer.Write(Value);
+
+            writer.BaseStream.Position += 24;
+        }
+        else if (Type == LogicStateChangeType.Reaction)
+        {
+            writer.Write(ReactionCoid);
+            writer.WriteTFID(Activator);
+            writer.Write(SingleClientOnly);
+
+            writer.BaseStream.Position += 7;
+        }
+        else
+            throw new InvalidDataException($"Unknown LogicStateChangeType {Type}!");
+    }
+}
