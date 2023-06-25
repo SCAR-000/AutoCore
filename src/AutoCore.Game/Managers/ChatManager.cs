e84ea5d0f4d56520087779c0684a4f4c3bf19dee
Blumster <sarkany0@hotmail.com>
2023-06-25 16:25:25 +0200

    Experiment with dropping loot on the ground with chat commands

--- src/AutoCore.Game/Managers/ChatManager.cs
+++ src/AutoCore.Game/Managers/ChatManager.cs
@@ -1,6 +1,7 @@
 ﻿namespace AutoCore.Game.Managers;
 
 using AutoCore.Game.Constants;
+using AutoCore.Game.Entities;
 using AutoCore.Game.Packets.Global;
 using AutoCore.Game.Packets.Sector;
 using AutoCore.Game.TNL;
@@ -15,7 +16,10 @@ public class ChatManager : Singleton<ChatManager>
         packet.Read(reader);
 
         if (packet.Message.StartsWith('/'))
+        {
+            HandleChatCommand(connection, packet.Message);
             return;
+        }
 
         switch (packet.ChatType)
         {
@@ -46,14 +50,88 @@ public class ChatManager : Singleton<ChatManager>
 
     public void HandleBroadcastPacket(TNLConnection connection, BinaryReader reader)
     {
-        var broadcastPacket = new BroadcastPacket();
-        broadcastPacket.Read(reader);
+        var packet = new BroadcastPacket();
+        packet.Read(reader);
 
-        if (broadcastPacket.Message.StartsWith('/'))
+        if (packet.Message.StartsWith('/'))
+        {
+            HandleChatCommand(connection, packet.Message);
             return;
+        }
 
-        connection.SendGamePacket(broadcastPacket);
+        connection.SendGamePacket(packet);
 
         // TODO: later: handle chat commands and send the chat packet to the proper recipient(s)
+    }
+
+    private void HandleChatCommand(TNLConnection connection, string command)
+    {
+        Logger.WriteLog(LogType.Debug, $"Conn {connection.Account.Name} sent chat command: {command}");
+
+        var parts = command.Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+        var respPacket = new BroadcastPacket
+        {
+            IsGM = false,
+            Sender = "System",
+            ChatType = ChatType.SystemMessage,
+            Message = ""
+        };
+
+        var character = connection.CurrentCharacter;
+
+        switch (parts[0])
+        {
+            case "/loot":
+                if (parts.Length < 2)
+                {
+                    respPacket.Message = $"Invalid loot command! Specify a cbid!";
+                    break;
+                }
+
+                if (int.TryParse(parts[1], out var cbid))
+                {
+                    var item = ClonedObjectBase.AllocateNewObjectFromCBID(cbid);
+                    if (item == null)
+                    {
+                        respPacket.Message = $"Unable to create item {cbid}!";
+                        break;
+                    }
+
+                    item.SetCoid(character.Map.LocalCoidCounter++, false);
+                    item.LoadCloneBase(cbid);
+                    item.Faction = character.Faction;
+                    item.Position = character.CurrentVehicle.Position;
+                    item.Rotation = character.CurrentVehicle.Rotation;
+
+                    CreateSimpleObjectPacket createPacket;
+                    switch (item)
+                    {
+                        case WheelSet:
+                            createPacket = new CreateWheelSetPacket();
+                            break;
+
+                        default:
+                            createPacket = null;
+                            break;
+                    }
+
+                    if (createPacket is not null)
+                    {
+                        item.WriteToPacket(createPacket);
+
+                        connection.SendGamePacket(createPacket);
+                    }
+                }
+                break;
+
+            default:
+                Logger.WriteLog(LogType.Debug, $"Unhandled chat command: {parts[0]}");
+                break;
+        }
+
+        respPacket.MessageLength = (short)respPacket.Message.Length;
+
+        connection.SendGamePacket(respPacket);
     }
 }
